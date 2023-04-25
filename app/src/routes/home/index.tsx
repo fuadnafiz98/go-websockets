f50fe1db7948b06894f331f3616a777d295c36d3
fuadnafiz98 <fuadnafiz98@gmail.com>
2023-04-25 21:44:54 +0600

    tailwind setup

--- app/src/routes/home/index.tsx
+++ app/src/routes/home/index.tsx
@@ -1,6 +1,10 @@
 import { h } from "preact";
 const Home = () => {
-  return <h2>Hello Chat!</h2>;
+  return (
+    <h2 className="text-2xl font-bold h-screen grid place-content-center">
+      Hello Chat!
+    </h2>
+  );
 };
 
 export default Home;
